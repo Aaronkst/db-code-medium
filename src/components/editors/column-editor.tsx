d02b2d974e9a20682adf37556e43429b25ec2201
Aaron Chen <dev.kst.aaron@gmail.com>
2025-02-03 00:41:41 +0900

    feat: shared inputs and more column options

--- src/components/editors/column-editor.tsx
+++ src/components/editors/column-editor.tsx
@@ -1,15 +1,48 @@
 import { EditorContext } from "@/lib/context/editor-context";
 import type { ColumnProps } from "@/utils/types/database-types";
 import { X } from "lucide-react";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { Button } from "../shared/buttons/button";
 import { IconButton } from "../shared/buttons/icon-button";
+import { Input, Select } from "../shared/inputs";
+
+const dataTypes = [
+  {
+    label: "String",
+    value: "string",
+  },
+  {
+    label: "Number",
+    value: "number",
+  },
+  {
+    label: "Date",
+    value: "date",
+  },
+  {
+    label: "JSON",
+    value: "json",
+  },
+  {
+    label: "Float",
+    value: "float",
+  },
+];
 
 export function ColumnEditor() {
   const { editingColumn, setEditingColumn, setEditingJoin } =
     useContext(EditorContext);
 
   if (!editingColumn) return <></>;
+
+  // TODO: handle `objectId` for mongodb.
+  const dataTypeOpts = useMemo(
+    () =>
+      editingColumn.primaryKey
+        ? [...dataTypes, { label: "UUID", value: "uuid" }]
+        : dataTypes,
+    [editingColumn.primaryKey],
+  );
 
   return (
     <form className="flex flex-col gap-3 p-2">
@@ -22,8 +55,8 @@ export function ColumnEditor() {
         />
       </div>
       <hr />
-      <label htmlFor="column-name">Name</label>
-      <input
+      <Input
+        label="Name"
         id="column-name"
         type="text"
         value={editingColumn.name}
@@ -33,13 +66,15 @@ export function ColumnEditor() {
         className="dark:bg-neutral-600 p-2"
       />
 
-      <label htmlFor="column-db-name">Name (Database)</label>
-      <span className="text-xs text-neutral-700 dark:text-neutral-400">
-        Encouraged to use <code className="text-pink-700">snake_case</code> for
-        RDS naming and <code className="text-pink-700">camelCase</code> for
-        NoSQL naming.
-      </span>
-      <input
+      <Input
+        label="Name (Database)"
+        description={
+          <>
+            Encouraged to use <code className="text-pink-700">snake_case</code>{" "}
+            for RDS naming and <code className="text-pink-700">camelCase</code>{" "}
+            for NoSQL naming.
+          </>
+        }
         id="column-name"
         type="text"
         value={editingColumn.name}
@@ -49,8 +84,8 @@ export function ColumnEditor() {
         className="dark:bg-neutral-600 p-2"
       />
 
-      <label htmlFor="column-data-type">Data Type</label>
-      <select
+      <Select
+        label="Data Type"
         id="column-data-type"
         value={editingColumn.dataType}
         onChange={(e) =>
@@ -60,131 +95,82 @@ export function ColumnEditor() {
           })
         }
         className="dark:bg-neutral-600 p-2"
-      >
-        <option value="string">String</option>
-        <option value="number">Number</option>
-        <option value="date">Date</option>
-        <option value="json">JSON</option>
-        <option value="float">Float</option>
-        {editingColumn.primaryKey && <option value="uuid">UUID</option>}
-        {/* TODO: Mongodb */}
-        {/* {editingColumn.primaryKey && <option value="objectId">Object ID</option>} */}
-      </select>
+        data={dataTypeOpts}
+      />
 
-      <div className="flex gap-2">
-        <input
-          id="column-primary-key"
-          type="checkbox"
-          checked={!!editingColumn.primaryKey}
-          onChange={() =>
-            setEditingColumn({
-              ...editingColumn,
-              primaryKey: !editingColumn.primaryKey,
-            })
-          }
-        />
-        <label htmlFor="column-primary-key">Primary Key</label>
-      </div>
+      <Input
+        label="Primary Key"
+        id="column-primary-key"
+        type="checkbox"
+        checked={!!editingColumn.primaryKey}
+        onChange={() =>
+          setEditingColumn({
+            ...editingColumn,
+            primaryKey: !editingColumn.primaryKey,
+          })
+        }
+      />
 
-      <div className="flex gap-2">
-        <input
-          id="column-auto-increment"
-          type="checkbox"
-          checked={!!editingColumn.autoIncrement}
-          onChange={() =>
-            setEditingColumn({
-              ...editingColumn,
-              autoIncrement: !editingColumn.autoIncrement,
-            })
-          }
-          disabled={
-            !editingColumn.primaryKey || editingColumn.dataType !== "number"
-          }
-        />
-        <label
-          htmlFor="column-auto-increment"
-          className={
-            !editingColumn.primaryKey
-              ? "text-neutral-400 dark:text-neutral-700"
-              : ""
-          }
-        >
-          Auto Increment
-        </label>
-      </div>
+      <Input
+        label="Auto Increment"
+        id="column-auto-increment"
+        type="checkbox"
+        checked={!!editingColumn.autoIncrement}
+        onChange={() =>
+          setEditingColumn({
+            ...editingColumn,
+            autoIncrement: !editingColumn.autoIncrement,
+          })
+        }
+        disabled={
+          !editingColumn.primaryKey || editingColumn.dataType !== "number"
+        }
+      />
 
-      <div className="flex gap-2">
-        <input
-          id="column-unique"
-          type="checkbox"
-          checked={!!editingColumn.unique}
-          onChange={() =>
-            setEditingColumn({
-              ...editingColumn,
-              unique: !editingColumn.unique,
-              nullable: false,
-            })
-          }
-        />
-        <label htmlFor="column-unique">Unique</label>
-      </div>
+      <Input
+        label="Unique"
+        id="column-unique"
+        type="checkbox"
+        checked={!!editingColumn.unique}
+        onChange={() =>
+          setEditingColumn({
+            ...editingColumn,
+            unique: !editingColumn.unique,
+            nullable: false,
+          })
+        }
+      />
 
-      <div className="flex gap-2">
-        <input
-          id="column-index"
-          type="checkbox"
-          checked={!!editingColumn.index}
-          onChange={() =>
-            setEditingColumn({ ...editingColumn, index: !editingColumn.index })
-          }
-        />
-        <label htmlFor="column-index">Index</label>
-      </div>
+      <Input
+        label="Index"
+        id="column-index"
+        type="checkbox"
+        checked={!!editingColumn.index}
+        onChange={() =>
+          setEditingColumn({ ...editingColumn, index: !editingColumn.index })
+        }
+      />
 
-      <div className="flex gap-2">
-        <input
-          id="column-nullable"
-          type="checkbox"
-          checked={!!editingColumn.nullable}
-          disabled={
-            editingColumn.primaryKey ||
-            editingColumn.autoIncrement ||
-            editingColumn.unique
-          }
-          onChange={() =>
-            setEditingColumn({
-              ...editingColumn,
-              nullable: !editingColumn.nullable,
-            })
-          }
-        />
-        <label
-          htmlFor="column-nullable"
-          className={
-            editingColumn.primaryKey ||
-            editingColumn.autoIncrement ||
-            editingColumn.unique
-              ? "text-neutral-400 dark:text-neutral-700"
-              : ""
-          }
-        >
-          Nullable
-        </label>
-      </div>
-
-      <label
-        htmlFor="column-default-value"
-        className={
+      <Input
+        label="Nullable"
+        id="column-nullable"
+        type="checkbox"
+        checked={!!editingColumn.nullable}
+        disabled={
           editingColumn.primaryKey ||
           editingColumn.autoIncrement ||
           editingColumn.unique
-            ? "text-neutral-400 dark:text-neutral-700"
-            : ""
         }
-      >
-        Default Value
-      </label>
-      <input
+        onChange={() =>
+          setEditingColumn({
+            ...editingColumn,
+            nullable: !editingColumn.nullable,
+          })
+        }
+      />
+
+      <Input
+        label="Default Value"
         id="column-default-value"
         type="text"
         value={editingColumn.defaultValue?.toString() || ""}
@@ -202,8 +188,8 @@ export function ColumnEditor() {
         }
       />
 
-      <label htmlFor="column-length">Length</label>
-      <input
+      <Input
+        label="Length"
         id="column-length"
         type="number"
         value={editingColumn.length}
@@ -217,58 +203,148 @@ export function ColumnEditor() {
       />
 
       {["float", "number"].includes(editingColumn.dataType) && (
-        <>
-          <label htmlFor="column-precision">Precision</label>
-          <input
-            id="column-precision"
-            type="number"
-            value={editingColumn.precision || 0}
-            onChange={(e) =>
-              setEditingColumn({
-                ...editingColumn,
-                precision: parseInt(e.target.value || "0"),
-              })
-            }
-            className="dark:bg-neutral-600 p-2"
-          />
-        </>
+        <Input
+          label="Precision"
+          id="column-precision"
+          type="number"
+          value={editingColumn.precision || 0}
+          onChange={(e) =>
+            setEditingColumn({
+              ...editingColumn,
+              precision: parseInt(e.target.value || "0"),
+            })
+          }
+          className="dark:bg-neutral-600 p-2"
+        />
       )}
 
       {editingColumn.dataType === "float" && (
-        <>
-          <label htmlFor="column-scale">Scale</label>
-          <input
-            id="column-scale"
-            type="number"
-            value={editingColumn.scale || 0}
-            onChange={(e) =>
-              setEditingColumn({
-                ...editingColumn,
-                scale: parseInt(e.target.value || "0"),
-              })
-            }
-            className="dark:bg-neutral-600 p-2"
-          />
-        </>
+        <Input
+          label="Scale"
+          id="column-scale"
+          type="number"
+          value={editingColumn.scale || 0}
+          onChange={(e) =>
+            setEditingColumn({
+              ...editingColumn,
+              scale: parseInt(e.target.value || "0"),
+            })
+          }
+          className="dark:bg-neutral-600 p-2"
+        />
       )}
 
       {editingColumn.dataType === "string" && (
-        <>
-          <label htmlFor="column-collation">Collation</label>
-          <input
-            id="column-collation"
-            type="number"
-            value={editingColumn.collation || 0}
-            onChange={(e) =>
-              setEditingColumn({
-                ...editingColumn,
-                collation: e.target.value || "",
-              })
-            }
-            className="dark:bg-neutral-600 p-2"
-          />
-        </>
+        <Input
+          label="Collation"
+          id="column-collation"
+          type="number"
+          value={editingColumn.collation || 0}
+          onChange={(e) =>
+            setEditingColumn({
+              ...editingColumn,
+              collation: e.target.value || "",
+            })
+          }
+        />
       )}
+
+      <Input
+        label="Select"
+        id="column-select"
+        type="checkbox"
+        checked={!!editingColumn.select}
+        onChange={() =>
+          setEditingColumn({
+            ...editingColumn,
+            select: !editingColumn.select,
+          })
+        }
+      />
+
+      {/* MySQL Only */}
+      <Input
+        label="Zerofill"
+        id="column-zerofill"
+        type="checkbox"
+        checked={!!editingColumn.zerofill}
+        onChange={() =>
+          setEditingColumn({
+            ...editingColumn,
+            zerofill: !editingColumn.zerofill,
+          })
+        }
+      />
+
+      {editingColumn.dataType === "string" && (
+        <Input
+          label="Enum"
+          description="Please enter enum values seperated by comma"
+          placeholder="value1, value2, etc..."
+          id="column-enum"
+          type="string"
+          value={editingColumn.enum?.length ? editingColumn.enum.join(",") : ""}
+          onChange={(e) =>
+            setEditingColumn({
+              ...editingColumn,
+              enum: e.target.value?.split(",") || null,
+            })
+          }
+        />
+      )}
+
+      {editingColumn.dataType === "string" && (
+        <Input
+          label="Enum name"
+          id="column-enum-name"
+          type="string"
+          value={editingColumn.enumName || 0}
+          onChange={(e) =>
+            setEditingColumn({
+              ...editingColumn,
+              enumName: e.target.value || "",
+            })
+          }
+        />
+      )}
+
+      {/* Postgres only */}
+      <Select
+        label="Hstore Type"
+        id="column-hstore-type"
+        value={editingColumn.hstoreType || ""}
+        onChange={(e) =>
+          setEditingColumn({
+            ...editingColumn,
+            hstoreType: e.target.value as ColumnProps["hstoreType"],
+          })
+        }
+        className="dark:bg-neutral-600 p-2"
+        data={[
+          {
+            label: "String",
+            value: "string",
+          },
+          {
+            label: "Object",
+            value: "object",
+          },
+        ]}
+      />
+
+      {/* Postgres and Cockroach db */}
+      <Input
+        label="Array"
+        id="column-array"
+        type="checkbox"
+        checked={!!editingColumn.array}
+        onChange={() =>
+          setEditingColumn({
+            ...editingColumn,
+            array: !editingColumn.array,
+          })
+        }
+      />
       <hr />
 
       {!!editingColumn.foreignKey && (
---
+++ src/components/shared/inputs/index.tsx
@@ -0,0 +1,108 @@
+import { classNames } from "@/utils/helpers";
+import type {
+  InputHTMLAttributes,
+  ReactNode,
+  SelectHTMLAttributes,
+} from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  description?: string | ReactNode;
+  id: string;
+}
+
+export function Input({
+  label,
+  description,
+  id,
+  type,
+  value,
+  checked,
+  onChange,
+  className,
+  ...props
+}: InputProps) {
+  return (
+    <div className={type === "checkbox" ? "flex flex-row-reverse gap-2" : ""}>
+      {label && (
+        <label
+          className={
+            props.disabled ? "text-neutral-400 dark:text-neutral-700" : ""
+          }
+          htmlFor={id}
+        >
+          {label}
+        </label>
+      )}
+      {description && (
+        <span className="text-xs text-neutral-700 dark:text-neutral-400">
+          {description}
+        </span>
+      )}
+      <input
+        {...props}
+        id={id}
+        type={type}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+        className={classNames("dark:bg-neutral-600 p-2", className || "")}
+      />
+    </div>
+  );
+}
+
+interface SelectProps
+  extends Omit<SelectHTMLAttributes<HTMLSelectElement>, "children"> {
+  label?: string;
+  description?: string | ReactNode;
+  id: string;
+  data: {
+    label: string;
+    value: string;
+  }[];
+}
+
+export function Select({
+  label,
+  description,
+  id,
+  value,
+  onChange,
+  className,
+  data,
+  ...props
+}: SelectProps) {
+  return (
+    <div>
+      {label && (
+        <label
+          className={
+            props.disabled ? "text-neutral-400 dark:text-neutral-700" : ""
+          }
+          htmlFor={id}
+        >
+          {label}
+        </label>
+      )}
+      {description && (
+        <span className="text-xs text-neutral-700 dark:text-neutral-400">
+          {description}
+        </span>
+      )}
+      <select
+        {...props}
+        id={id}
+        value={value}
+        onChange={onChange}
+        className={classNames("dark:bg-neutral-600 p-2", className || "")}
+      >
+        {data.map(({ label, value }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
